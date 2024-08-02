655fa8419d60a3a092c82820733e4c4a090cefb0
Jakub Sieńko <jakub.sienko@dazn.com>
2024-08-02 17:27:30 +0200

    add sagemcom and webos and platforms, add help strings

---
+++ parser.py
@@ -0,0 +1,9 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='Suitest Autoclicker',
+                    description='Translates strings to clicks on remote to input them on the device'
+                    )
+
+parser.add_argument('platform', choices=['sky', 'sagemcom', 'webos'], help="Platform of the device.")
+parser.add_argument('string', help="Text that should be translated.")
