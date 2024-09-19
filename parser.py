b3f6e6ef3afcb87c1413a78c98ee7d96bf461809
Jakub Sieńko <jakub.sienko@dazn.com>
2024-09-19 10:45:20 +0200

    add TODO

--- parser.py
+++ parser.py
@@ -6,5 +6,7 @@ parser = argparse.ArgumentParser(
                     description='Translates strings to clicks on remote to input them on the device'
                     )
 
+# TODO can be created by reading contents of keyboards file instead of filling them manually, this will
+# remove need of checking if file with given name exists
 parser.add_argument('platform', choices=['sky', 'sagemcom', 'webos'], help="Platform of the device.")
 parser.add_argument('string', help="Text that should be translated.")
