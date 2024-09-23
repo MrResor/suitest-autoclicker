a699dad5fa3c19174e4866ab2ffd1798affe90c8
Jakub Sieńko <jakub.sienko@dazn.com>
2024-09-23 08:11:37 +0200

    change help text format for platform and string inputs

--- parser.py
+++ parser.py
@@ -8,5 +8,5 @@ parser = argparse.ArgumentParser(
 
 # TODO can be created by reading contents of keyboards file instead of filling them manually, this will
 # remove need of checking if file with given name exists
-parser.add_argument('platform', choices=['sky', 'sagemcom', 'webos'], help="Platform of the device.")
-parser.add_argument('string', help="Text that should be translated.")
+parser.add_argument('platform', choices=['sky', 'sagemcom', 'webos'], help="platform of the device")
+parser.add_argument('string', help="text that should be translated")
