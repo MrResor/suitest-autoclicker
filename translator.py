6b28a0606847695780eb54a3659fd3b9f26d074d
Jakub Sieńko <jakub.sienko@dazn.com>
2024-09-23 10:31:12 +0200

    rearange functions and descriptions into alphabetical order

--- translator.py
+++ translator.py
@@ -13,30 +13,31 @@ class Translator():
 
         Attributes:\n
         _args                           -- dictionary with arguments parsed by argparse.\n
+        _commands                       -- string created using the list of commands that need to be used in
+        order to input the string on the deivce, it is either printed to console or coppied into clipboard
+        and it has to be coppied into test in suite.st.\n
         _format                         -- format of tqdm bar.\n
         _keyboard                       -- disctionary holding keyboard layouts and additional informations
         needed by a program to run the BFS calculations.\n
         _text                           -- string given by the user, the same variable is used to perform all
         the necessary transformations.\n
-        _commands                       -- string created using the list of commands that need to be used in
-        order to input the string on the deivce, it is either printed to console or coppied into clipboard
-        and it has to be coppied into test in suite.st.\n
 
         Methods:\n
-        _read_keyboard_setup            -- reads respecitve keyboard file and parses it into dictionary to
-        be used by other functions of this program.\n
-        _input_prep                     -- converts string to symbols that can be found on the keybord ([A]
-        is changed into [SHIFT, a], any special symbols are prefixed with SPECIAL symbol).\n
         run                             -- runs the main algorithm of the program using the previously
         prepaired text input and producing the final output of the program.\n
-        _prep_commands                  -- 
         _bfs                            -- implementation of Breadth First Search algorithm, finds the
         shortest path between two nodes in the graph.\n
+        _input_prep                     -- converts string to symbols that can be found on the keybord ([A]
+        is changed into [SHIFT, a], any special symbols are prefixed with SPECIAL symbol).\n
+        _prep_commands                  -- main function responsible for compiling the list of commands
+        needed by suitest to type in the given string on the chosen device.\n
+        _read_keyboard_setup            -- reads respecitve keyboard file and parses it into dictionary to
+        be used by other functions of this program.\n
         _to_clipboard                   -- coppies result to clipboard or prints it to the console based on
-        the system the program was used on.\n
-        _handle_win_and_linux_output    -- handles the printing of the result on windows and linux platforms.
+        the system the program was used on.
     """
 
+    # public functions
     def __init__(self) -> None:
         """ Sets up translator and constants that are used later on. Calls for initial parsing of the input
             string.
@@ -46,11 +47,50 @@ class Translator():
         self._read_keyboard_setup()
         self._input_prep()
 
-    def _read_keyboard_setup(self) -> None:
-        """ Read keyboard .json file and parse it into JSON / dictionary.
+    def run(self) -> None:
+        """ Prepaires the commands to be used by suitest and puts them into clipboard.
         """
-        with open("keyboards/" + self._args.platform + ".json") as f:
-            self._keyboard = json.loads("".join(f.readlines()))
+        self._prep_commands()
+        self._to_clipboard()
+
+    # private functions
+    def _bfs(self, graph:dict, S:str, D:str) -> None:
+        """ Implementation of Breadth First Search algorithm for finding shortest path in the graph. Takes
+            in graph description, starting and destination nodes as parameters and returns a list of nodes
+            that needs to be visited in given order to reach destination node from the starting node.
+        """
+
+        visited = {}
+        # Queue to store the nodes in the order they are visited
+        q = deque()
+        # Push the source node to the queue
+        q.append(S)
+
+        visited[S] = None
+
+        # Iterate until the queue is not empty
+        while q:
+            # Pop the node at the front of the queue
+            node = q.popleft()
+            # Explore all the neighbors of the current node
+            if node == D:
+                path = []
+                while node:
+                    path.append(node)
+                    node = visited[node]
+                return path [::-1]
+
+            for neighbour in graph[node]:
+                # Check if the neighboring node is not visited
+                if neighbour not in visited:
+                    visited[neighbour] = node
+                    q.append(neighbour)
+
+    def _handle_win_and_linux_output(self) -> None:
+        """ Informs that copying to clipboard is not possible and prints to console.
+        """
+        print("Platform not supported for copying to clipboard, please copy manualy from below:")
+        print(self._commands)
 
     def _input_prep(self) -> None:
         """ Parses the input string based on the keyboard type so it can be typed in using given keyboard.
@@ -96,12 +136,6 @@ class Translator():
                 text.insert(0, "SPECIAL")
         self._text = text
 
-    def run(self) -> None:
-        """ Prepaires the commands to be used by suitest and puts them into clipboard.
-        """
-        self._prep_commands()
-        self._to_clipboard()
-
     def _prep_commands(self) -> None:
         """ Prepaires the commands that should be send to suitest to type in given string. It paires the
             letters in the string (for example 'ABC' is converted into pairs 'AB' and 'BC') and then finds
@@ -135,39 +169,13 @@ class Translator():
             commands.append(f'{{"type":"button","excluded":false,"fatal":false,"screenshot":false,"count":1,"delay":"<%del%>","ids":["ENTER"]}}')
             special = not special if i[1] == "SPECIAL" else special
                 
-        self._commands = '[' + ','.join(commands) + ']'
+        self._commands = '[' + ','.join(commands) + ']'    
 
-    def _bfs(self, graph:dict, S:str, D:str) -> None:
-        """ Implementation of Breadth First Search algorithm for finding shortest path in the graph. Takes
-            in graph description, starting and destination nodes as parameters and returns a list of nodes
-            that needs to be visited in given order to reach destination node from the starting node.
+    def _read_keyboard_setup(self) -> None:
+        """ Read keyboard .json file and parse it into JSON / dictionary.
         """
-
-        visited = {}
-        # Queue to store the nodes in the order they are visited
-        q = deque()
-        # Push the source node to the queue
-        q.append(S)
-
-        visited[S] = None
-
-        # Iterate until the queue is not empty
-        while q:
-            # Pop the node at the front of the queue
-            node = q.popleft()
-            # Explore all the neighbors of the current node
-            if node == D:
-                path = []
-                while node:
-                    path.append(node)
-                    node = visited[node]
-                return path [::-1]
-
-            for neighbour in graph[node]:
-                # Check if the neighboring node is not visited
-                if neighbour not in visited:
-                    visited[neighbour] = node
-                    q.append(neighbour)    
+        with open("keyboards/" + self._args.platform + ".json") as f:
+            self._keyboard = json.loads("".join(f.readlines()))
 
     def _to_clipboard(self) -> None:
         """ Copies prepared instructions to clipboard, and if copying is not possible it will call a function
@@ -181,10 +189,3 @@ class Translator():
             print("Coppied to clibpoard.")
         elif platform == "win32":
             self._handle_win_and_linux_output()
-    
-    def _handle_win_and_linux_output(self) -> None:
-        """ Informs that copying to clipboard is not possible and prints to console.
-        """
-        print("Platform not supported for copying to clipboard, please copy manualy from below:")
-        print(self._commands)
-        
