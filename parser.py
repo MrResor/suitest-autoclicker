390573fa129f1e69f32ae79427fbe86db4bfc2d5
Jakub Sieńko <jakub.sienko@dazn.com>
2024-12-02 21:30:40 +0100

    autopopulate choices of platform argument using names of .json files in keyboard folder in ascending order

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import argparse
+from os import walk
 
 # setup the parser for autoclicker arguments
 parser = argparse.ArgumentParser(
@@ -6,7 +7,9 @@ parser = argparse.ArgumentParser(
                     description='Translates strings to clicks on remote to input them on the device'
                     )
 
-# TODO can be created by reading contents of keyboards file instead of filling them manually, this will
-# remove need of checking if file with given name exists
-parser.add_argument('platform', choices=['sky', 'sagemcom', 'webos'], help="platform of the device")
+# checks json files in keyboard folders and uses them to populate choices of platform argument
+filenames = next(walk('./keyboards'), (None, None, []))[2] 
+choices = [filename.split('.json')[0] for filename in filenames if filename.endswith('.json')]
+choices.sort()
+parser.add_argument('platform', choices=choices, help="platform of the device")
 parser.add_argument('string', help="text that should be translated")
